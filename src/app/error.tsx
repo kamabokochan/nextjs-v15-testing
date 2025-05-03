a711a8da821f0a85508d7fe40150c8f114dbca9b
kamabokochan <h.sasaki.worksfront@gmail.com>
2025-05-04 00:10:32 +0900

    エラーページと404ページのコンポーネントを追加し、ユーザ情報取得関数を実装

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+// error.tsxは、クライアントコンポーネントとして定義する必要がある。
+"use client";
+
+export default function Error() {
+	return (
+		<main>
+			<h1>エラーが発生しました</h1>
+			<p>お探しのページは見つかりませんでした。</p>
+			<p>URLが正しいか、ページが削除された可能性があります。</p>
+		</main>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { getUserInfo } from "@/api/userInfo";
+import { getUserInfo } from "@/data/userInfo";
+import Link from "next/link";
 
 export default async function Home() {
 	const userInfo = await getUserInfo();
+
+	// error.tsx 動作確認用
+	// throw new Error("Errorが発生しました");
+
 	return (
 		<main>
+			<h1>ユーザ情報を表示</h1>
 			<ul>
 				<li>{userInfo.user}</li>
 				<li>{userInfo.age}</li>
@@ -11,6 +17,7 @@ export default async function Home() {
 					<a href={userInfo.link}>{userInfo.link}</a>
 				</li>
 			</ul>
+			<Link href="/404">404ページ</Link>
 		</main>
 	);
 }
