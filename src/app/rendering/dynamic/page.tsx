7f53e1d8a0e747f69c77af3a3f1fafb82cd48bbd
kamabokochan <h.sasaki.worksfront@gmail.com>
2025-05-06 01:01:40 +0900

    dayjsを追加し、APIから取得した時間を動的および静的ページで表示する機能を実装

--- src/app/rendering/dynamic/page.tsx
+++ src/app/rendering/dynamic/page.tsx
@@ -1,19 +1,16 @@
-// https://nextjs.org/docs/app/building-your-application/rendering/server-components#static-rendering-default
-
 import { request } from "@/utils/request";
 
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
 	const response = await request<{ time: string }>(
 		`http://localhost:3000/api/getTime`,
-		{ cache: "force-cache" },
 	);
 
 	return (
 		<div>
-			<h1>Static Page</h1>
-			<p>This page is statically rendered.</p>
+			<h1>Dynamic Page</h1>
+			<p>This page is dynamically rendered.</p>
 			<p>request time: {response.data?.time}</p>
 		</div>
 	);
