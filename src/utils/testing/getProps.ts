131c5662174657b0d699650878286c858e08117b
kamabokochan <h.sasaki.worksfront@gmail.com>
2025-05-24 03:46:50 +0900

    getPropsを変更 - 兄弟要素に対応 - 自分が読みやすい形に書き換え

--- src/utils/testing/getProps.ts
+++ src/utils/testing/getProps.ts
@@ -20,23 +20,41 @@ function isElement(prop: unknown): prop is JSX.Element {
 	return false;
 }
 
+function flattenChildren(children: unknown): JSX.Element[] {
+	if (!children) {
+		return [];
+	}
+	const elements: JSX.Element[] = [];
+
+	const exploreChild = (child: unknown) => {
+		if (isElement(child)) {
+			elements.push(child);
+			Object.values(child.props).forEach(exploreChild); // 再帰的に子を探索
+		} else if (Array.isArray(child)) {
+			child.forEach(exploreChild);
+		}
+	};
+	exploreChild(children);
+	return elements;
+}
+
 export function getProps<
 	T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>,
 >(
 	element: JSX.Element,
 	componentType: Function | string,
 ): ComponentProps<T> | undefined {
+	// 渡されたelement.typeが一致する場合は、そのpropsを返す
 	if (element.type === componentType) {
 		return element.props;
 	}
-	const foundProps = Object.values(element.props).reduce(
-		(acc: object[], prop) => {
-			if (!isElement(prop)) return acc;
-			const hit = getProps(prop, componentType);
-			if (!hit) return acc;
-			return [...acc, hit];
-		},
-		[],
-	);
-	return foundProps[0] as any;
+	// 兄弟を含む子要素を一次元配列にする
+	const children = flattenChildren(element.props.children);
+	const foundProps = children.find((child) => {
+		if (child.type === componentType) {
+			return child;
+		}
+	});
+
+	return foundProps?.props;
 }
